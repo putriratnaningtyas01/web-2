e5a30829c1a7dd6fdcf0bb605486c7a3cd48c01b
putriratnaningtyas01 <0110124152@student.nurulfikri.ac.id>
2025-03-04 22:04:39 +0700

    tugas 2

---
+++ praktikum-2a/data-form-regis.php
@@ -0,0 +1,22 @@
+<?php
+#array associative
+$ar_prodi = [
+    'SI' => "Sistem Informasi",
+    'TI' => "Teknik Informatika",
+    'BD' => "Bisnis Digital"
+];
+
+#array associative
+$ar_skill = [
+    'HTML' => 10,
+    'CSS' => 10,
+    'JavaScript' => 20,
+    'RWD Bootstrap' => 20,
+    'PHP' => 30,
+    'Python' => 30,
+    'Java' => 50
+];
+
+#array skalar/index
+$ar_domisili = 
+["Jakarta", "Depok", "Bogor", "Tangerang", "Bekasi", "Lainnya"];
