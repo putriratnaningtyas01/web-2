1022a3dd749618f972476b27719fb08673bd3522
putriratnaningtyas01 <0110124152@student.nurulfikri.ac.id>
2025-06-07 21:46:37 +0700

    pertemuan 11

---
+++ pertemuan11/filament-app/app/Filament/Resources/MahasiswaResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MahasiswaResource\Pages;
+use App\Filament\Resources\MahasiswaResource\RelationManagers;
+use App\Models\Mahasiswa;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+
+class MahasiswaResource extends Resource
+{
+    protected static ?string $model = Mahasiswa::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('nama')->searchable()->sortable(),
+                TextColumn::make('nim')->searchable(),
+                TextColumn::make('jurusan')->searchable(),
+                TextColumn::make('jenis_kelamin')->searchable(),
+                TextColumn::make('agama')->searchable(),
+                TextColumn::make('status')->searchable()
+            ])
+            ->filters([
+                //nama
+                SelectFilter::make('nama')
+                    ->options([
+                        'Budi Santoso' => 'Islam',
+                        'Siti Aminah' => 'Kristen',
+                        'Andi Wijaya' => 'Hindu',
+                    ]),
+
+                //nim
+                SelectFilter::make('nim')
+                    ->options([
+                        '1234567890' => '1234567890',
+                        '0987654321' => '0987654321',
+                        '1122334455' => '1122334455',
+                    ]),
+
+                //jurusan
+                SelectFilter::make('jurusan')
+                    ->options([
+                        'Teknik Informatika' => '1234567890',
+                        'Sistem Informasi' => '0987654321',
+                        'Teknik Elektro' => '1122334455',
+                    ]),
+                    
+                //jenis_kelamin
+                SelectFilter::make('jenis_kelamin')
+                    ->options([
+                        'L' => 'Laki-Laki',
+                        'P' => 'Perempuan',
+                    ]),
+
+                //agama
+                SelectFilter::make('agama')
+                    ->options([
+                        'Islam' => 'Islam',
+                        'Kristen' => 'Kristen',
+                        'Hindu' => 'Hindu',
+                    ]),
+                
+                //status
+                SelectFilter::make('status')
+                    ->options([
+                        'aktif' => 'aktif-Laki',
+                        'tidak Aktif' => 'Perempuan',
+                    ])
+
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMahasiswas::route('/'),
+            'create' => Pages\CreateMahasiswa::route('/create'),
+            'edit' => Pages\EditMahasiswa::route('/{record}/edit'),
+        ];
+    }
+}
